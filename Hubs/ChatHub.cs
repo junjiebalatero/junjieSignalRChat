667d64dd310ace9380e57606d09f836c3a674bc4
marge <junjiebalatero@gmail.com>
2020-05-27 14:36:42 +0800

    first commit

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace SignalRChat.Hubs
+//{
+//    public class ChatHub
+//    {
+
+//    }
+//}
+
+namespace SignalRChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
